a6a0aa451c29b2aec880a509099a6e9b0d73fdd9
Aneisha Kadir <nuraneisha9@gmail.com>
2025-07-28 21:30:51 +0800

    remove debugging

--- src/components/Sucess.jsx
+++ src/components/Sucess.jsx
@@ -10,11 +10,8 @@ export default function Sucess() {
     const { currentUser } = useContext(AuthContext)
 
     useEffect(() => {
-        console.log("hi");
         const fetchSuccess = async (sessionId, user_id) => {
             try {
-                console.log("hi2", user_id);
-                console.log("session", sessionId);
                 const response = await fetch(`https://syx-project.vercel.app/success?session_id=${sessionId}&user_id=${user_id}`);
                 if (response.ok) {
                     setTimeout(() => {
@@ -25,20 +22,13 @@ export default function Sucess() {
                     console.error("Something went worng", response.status)
 
                 }
-
-                console.log("hi3");
             }
             catch (error) {
                 console.error("Error in fetching the data", error)
             }
         }
 
-
-
-        console.log("hi4");
-
         if (sessionId && currentUser) {
-            console.log("hi5");
             fetchSuccess(sessionId, currentUser.uid);
         }
     }, [currentUser, location, navigate]);
