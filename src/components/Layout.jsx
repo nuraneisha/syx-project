683673c24226df9d8aad892f8b659d991d74acc6
Aneisha Kadir <nuraneisha9@gmail.com>
2025-07-28 13:44:49 +0800

    fix modal issue

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,12 @@
 import Header from "./Header";
 import Footer from "./Footer";
-
+import Login from "../pages/Login"
+import { ModalContext } from "../context/ModalProvider";
+import { useContext } from "react"
 
 //to make Header always on top and Footer always at the bottom
 export default function Layout({ children }) {
+    const { showLoginModal } = useContext(ModalContext);
 
     return (
         <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
@@ -12,6 +15,7 @@ export default function Layout({ children }) {
                 {children}
             </div>
             <Footer />
+            {showLoginModal && <Login />}
         </div>
     );
 }
