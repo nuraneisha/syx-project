c8388ba6f328c657558a23760e62ccaf725be5f1
Aneisha Kadir <nuraneisha9@gmail.com>
2025-07-29 17:43:05 +0800

    fix it

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,11 +2,10 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Login from "../pages/Login"
 import { ModalContext } from "../context/ModalProvider";
-import { useContext } from "react"
 
 //to make Header always on top and Footer always at the bottom
 export default function Layout({ children }) {
-    const { showLoginModal } = useContext(ModalContext);
+
 
     return (
         <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
@@ -15,8 +14,7 @@ export default function Layout({ children }) {
                 {children}
             </div>
             <Footer />
-            <button onClick={() => showLoginModal(true)}>Show Login Modal</button>
-            {showLoginModal && <Login />}
+            <Login />
         </div>
     );
 }
